665cd6b7bc13d98362be99575aa8fbdbca65f48b
jzqiu <qiujz@hotmail.com>
2013-10-23 22:58:19 +0800

    我的指标填报列表

---
+++ DangJian/Controllers/QuotaController.cs
@@ -0,0 +1,54 @@
+﻿using DangJian.Models;
+using DangJian.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DangJian.Controllers
+{
+    public class QuotaController : Controller
+    {
+        DJContext ctx = new DJContext();
+        //
+        // GET: /Quota/
+
+        public ActionResult Index()
+        {
+            return View(ctx.QuotaGroups
+                .OrderBy(g=>g.Seq)
+                .ToList());
+        }
+
+        [ChildActionOnly]
+        public ActionResult QuotaList(string groupCode)
+        {
+            User user = Session["UserInfo"] as User;
+            if (user == null)
+            {
+                return RedirectToAction("Index", "Account");
+            }
+
+            var records = from r in ctx.QuotaRecords
+                          where r.CreateUser == user.UserId
+                          select new
+                          {
+                              r.GUID,
+                              r.QuotaCode
+                          };
+            var list = from q in ctx.Quotas
+                       join r in records on q.Code equals r.QuotaCode into g
+                       where q.GroupCode == groupCode
+                       orderby q.Seq
+                       from gg in g.DefaultIfEmpty()
+                       select new QuotaRecordVM
+                       {
+                           Description = q.Description,
+                           FillInfo = gg.GUID == null ? "未填报" : "已填报"
+                       };
+
+            return PartialView("_QuotaListPartial", list.ToList());
+        }
+    }
+}
---
+++ DangJian/ViewModels/QuotaRecordVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DangJian.ViewModels
+{
+    public class QuotaRecordVM
+    {
+        public string Description { get; set; }
+        public string FillInfo { get; set; }
+    }
+}
